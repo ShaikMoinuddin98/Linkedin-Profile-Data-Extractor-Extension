0dcce11880213968269802b51b8ddae480d47e9f
Shaik Moinuddin <130903025+ShaikMoinuddin98@users.noreply.github.com>
2024-05-10 19:47:47 +0530

    Add files via upload

---
+++ chrome ext server/index.js
@@ -0,0 +1,117 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+
+
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('atg', 'root', 'Moin7396', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+const { DataTypes } = require('sequelize');
+
+//model
+const Profile = sequelize.define('Profile', {
+    name: {
+        type: DataTypes.STRING,
+        primaryKey: true
+    },
+    link: {
+        type: DataTypes.STRING,
+
+    },
+    about: {
+        type: DataTypes.TEXT
+    },
+    bio: {
+        type: DataTypes.TEXT
+    },
+    location: {
+        type: DataTypes.STRING
+    },
+    followers: {
+        type: DataTypes.INTEGER
+    },
+    connections: {
+        type: DataTypes.INTEGER
+    },
+},
+    {
+        timestamps: false
+
+    });
+
+
+
+app.use(bodyParser.json());
+
+//allowing cors access from any site
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+//function for extracting number from text
+function extractNumber(text) {
+    // Check if text is a string
+    if (typeof text !== 'string') {
+        return null;
+    }
+
+    // Remove commas and non-numeric characters from the text
+    const sanitizedText = text.replace(/[^\d]/g, '');
+
+    // Parse the number from the sanitized text
+    const number = parseInt(sanitizedText, 10);
+
+    // If a valid number is parsed, return it; otherwise, return null
+    return isNaN(number) ? null : number;
+}
+
+
+//api for saving profile data
+app.post("/linkedinsave", async (req, res) => {
+    try{
+    //our three links which will be opened
+    // let links = ["https://www.linkedin.com/in/daniil-rabizo/", "https://www.linkedin.com/in/raziel121/", "https://www.linkedin.com/in/frazmohammad/"]
+    console.log(req.body)
+    //getting index of present link in window
+    // let ind = links.indexOf(req.body.link)
+    let { name, link, bio, about, location, connections, followers } = req.body
+    connections = extractNumber(connections)
+    followers = extractNumber(followers)
+    //saving the data
+    const profile = await Profile.create({
+        name,
+        link,
+        about,
+        bio,
+        location,
+        connections,
+        followers
+    });
+    console.log(profile)
+
+    //sending the next link in response
+    // res.status(200).json({ "message": "data posted", "link": links[ind + 1] })
+    res.status(200).json({"message":"data extracted"})
+}
+catch(err)
+{
+    console.log(err)
+    res.status(400).json({"message":err.message})
+}
+
+})
+
+
+
+app.listen("9000", () => {
+    console.log("listening")
+})
---
+++ chrome ext/popup.js
@@ -0,0 +1,70 @@
+// popup.js
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const extractDataBtn = document.getElementById('extractDataBtn');
+
+  extractDataBtn.addEventListener('click', function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      console.log(tabs)
+      if (tabs && tabs.length > 0) {
+        const activeTab = tabs[0];
+        console.log(tabs)
+
+        chrome.scripting.executeScript({
+
+          target: { tabId: activeTab.id },
+          function: extractDataFromLinkedInProfiles,
+
+        });
+
+
+      }
+      else {
+        console.error('No active tab found.');
+      }
+    });
+  });
+});
+
+function extractDataFromLinkedInProfiles() {
+  // Function to extract data from LinkedIn profiles
+  //extracting the data using selectors from linkedin profile page
+  let name = document.querySelector(".t-24")
+  let connections = document.querySelector(".DcGtcHGQtTkqBnYaUnNwMbnjThhhnAiGslM")
+  let connects = connections.childNodes[4].innerText
+  let about = document.querySelector(".khBnRjVIXCYirVFIMdnfnUqaXbtNZPxGUdysw")
+  let bio = document.querySelector(".CAAEaVJykYggesIuSfNeQJIlaxXPywBCqmk").childNodes[5].innerText
+  let location = document.querySelectorAll(".CAAEaVJykYggesIuSfNeQJIlaxXPywBCqmk")[1].childNodes[3].innerText
+  let followers = document.querySelector(".DDqKBVMyAjecFPGbPZGpRLhziEzsbKMdeMk").childNodes[1].innerText
+
+
+  //sending the data to our server
+  fetch('http://localhost:9000/linkedinsave', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ "name": name.innerText, "link": window.location.href, "connections": connects, "about": about.innerText, "bio": bio, "location": location, "followers": followers })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to post data to the server');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Data posted successfully:', data);
+      alert('Data posted successfully!');
+      // window.location.href = data.link
+
+    })
+    .catch(error => {
+      console.error('Error posting data:', error);
+      alert('Failed to post data to the server');
+    });
+
+
+}
+
